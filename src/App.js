276cb19dd54e117138e5918729ac2e21472c5bf3
Elizabeth Alanis <elizabeth.clausen@student.uagc.edu>
2024-11-18 19:55:50 -0800

    Week 2 assignment: Add employee form

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import EmployeeForm from './Components/EmployeeForm';
+
+function App() {
+  return (
+    <EmployeeForm />
+  );
+}
+
+export default App;
---
+++ src/Components/EmployeeForm.js
@@ -0,0 +1,62 @@
+import '../EmployeeForm.css';
+import React from 'react';
+
+class EmployeeForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {name: '', email: '', phone: ''};
+    }
+
+   handleChange = (e) => {
+        const { id, value } = e.target;
+        this.setState({ [id]: value });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+        this.setState({
+            name: '',
+            email: '',
+            phone: ''
+        });
+    }
+
+    render() {
+        return (
+            <form className="employee-form" onSubmit={this.handleSubmit}>   
+                <h2>Add Employee</h2>
+                <div>
+                    Name: <input
+                        type="text"
+                        id="name"
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                        placeholder="Name"
+                    />
+                </div>
+                <div>
+                    Email: <input
+                        type="email" 
+                        id="email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                        placeholder="Email"
+                    />
+                </div>
+                <div>
+                    Phone: <input
+                        type="tel"
+                        id="phone"
+                        value={this.state.phone}
+                        onChange={this.handleChange}
+                        placeholder="Phone"
+                    />
+                </div>
+                <button type="submit">Add</button>
+            </form>
+        );
+    }
+}
+
+export default EmployeeForm;
